9fd0776fe7bbd2b67e486dc227b5bc6d421d59b1
Hardik-Ahi <ahi.hardik@gmail.com>
2024-05-21 12:02:56 +0530

    initial working state

---
+++ signaling_server/server.js
@@ -0,0 +1,74 @@
+const express = require('express')
+const http = require('http')  // built-in module
+const socketIO = require('socket.io')
+const port = 5000
+const cors = require('cors')
+
+const app = express()
+app.use(cors({
+    origin: ['https://192.168.0.106:3000', 'https://localhost:3000']
+}))
+
+const server = http.createServer(app);  // 'app' is actually a function => callback function
+// 'app' can be passed to both http and https, thus we can use the same app configuration on http and https.
+
+const io = socketIO(server, {
+    cors: {
+        origin: ['https://192.168.0.106:3000', 'https://localhost:3000']
+    }
+});
+let clientOffer = null;
+let clientAnswer = null;
+let offererCandidates = [];
+let answererCandidates = [];
+
+io.on('connection', (socket) => {
+    console.log("some one connected");
+    socket.emit("offerAvailable", {status: clientOffer === null ? true: false, offer: clientOffer});
+
+    socket.on('disconnect', (reason) => {
+        console.log(reason);
+    });
+    
+    socket.on('offer', (offer) => {  // custom event using socket.emit('offer', offer) => offer is the thing we need
+        if(clientOffer === null){
+            console.log("first offer received");
+            clientOffer = offer;
+            socket.broadcast.emit("offerAvailable", {status: clientOffer === null ? true: false, offer: clientOffer});
+        }
+        else{
+            console.log("second client trying to create offer (not permitted)");
+        }
+    })
+
+    socket.on('answer', (answer) => {
+        if(clientOffer === null){
+            console.log("trying to create answer before offer created (not permitted)");
+        }
+        else{
+            console.log("answer received: sewing complete");
+            clientAnswer = answer;
+            socket.broadcast.emit('answer', clientAnswer);  // offerer should now get the answer
+        }
+    })
+
+    socket.on('iceCandidates', (object) => {
+        const offerer = object.offerer;
+        if(offerer){
+            offererCandidates = [...object.candidates];
+        }
+        else{
+            answererCandidates = [...object.candidates];
+        }
+        console.log("received some ice candidates");
+        console.log("lengths: "+offererCandidates.length+" "+answererCandidates.length+" from " + offerer);
+        if(offererCandidates.length > 0 && answererCandidates.length > 0){
+            console.log("starting to emit");
+            io.emit('iceCandidates', {offererCandidates: offererCandidates, answererCandidates: answererCandidates});
+        }
+    })
+})
+
+server.listen(port, () => {
+    console.log(`server listening on ${port}`);
+})
--- src/App.js
+++ src/App.js
@@ -1,25 +1,143 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useRef, useState} from 'react';
+import {io} from 'socket.io-client';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const localStream = useRef(null);  // for directly manipulating DOM elements; otherwise they are considered react components
+  const remoteStream = useRef();
+  const remoteTrack = useRef(new MediaStream());
+  const connection = useRef(null);  // RTCPeerConnection
+  const offer = useRef(null);
+
+  const socket = useRef(null);
+  const myCandidates = useRef([]);
+  const [canOffer, setCanOffer] = useState(false);  // using 'useState' because change in this should trigger re-render (button)
+  const didIOffer = useRef(false);
+  
+  useEffect(() => {
+    const constraints = {
+      audio: true,
+      video: true
+    }
+    const requestPermission = async() => {
+      navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        localStream.current.srcObject = stream;
+        console.log("got media stream");
+        
+      }, (reasonForFailure) => {
+        console.log(reasonForFailure);
+      })
+    }
+    
+    requestPermission();
+    socket.current = io('http://localhost:5000')
+
+    socket.current.on('offerAvailable', (status) => {
+      setCanOffer(status.status);
+      offer.current = status.offer;
+    })
+
+    socket.current.on('answer', (answer) => {
+      console.log("I received an answer to my offer");
+      connection.current.setRemoteDescription(answer).then(() => {
+        console.log("offerer set remote description");
+      })
+    })
+
+    socket.current.on('iceCandidates', (object) => {
+      if(didIOffer.current){
+        for(const c of object.answererCandidates){
+          connection.current.addIceCandidate(c).then(() => console.log("answerer's candidate added"));
+        }
+      }
+      else{
+        for(const c of object.offererCandidates){
+          connection.current.addIceCandidate(c).then(() => console.log("offerer's candidate added"));
+        }
+      }
+    })
+
+    return () => {
+      socket.current.disconnect();
+    }
+  }, [])
+
+  const onOpenConnection = (event) => {
+    connection.current = new RTCPeerConnection({
+      iceServers: [{
+        urls: ["stun:stun.l.google.com:19302", "stun:stun1.l.google.com:19302", "stun:stun2.l.google.com:19302"]
+      }]
+    });
+
+    connection.current.addEventListener("icegatheringstatechange", (event) => {
+      switch(event.target.iceGatheringState){
+        case "gathering":
+          console.log("switched to gathering more candidates.");
+          break;
+        case "complete":
+          console.log("finished gathering ice candidates.");
+          // emit the array to the server
+          socket.current.emit('iceCandidates', {offerer: didIOffer.current, candidates: myCandidates.current});
+          break;
+        default:
+          break;
+      }
+    })
+
+    connection.current.addEventListener("icecandidate", (event) => {
+      if(event.candidate !== null){
+        myCandidates.current.push(event.candidate);
+        console.log("========== found candidate: " + event.candidate);
+      }
+    })
+
+
+    localStream.current.srcObject.getTracks().forEach((item, index) => {
+      console.log(item);
+      connection.current.addTrack(item);
+    })
+
+    console.log("tracks added");
+
+    
+    if(canOffer){
+      connection.current.createOffer().then((offer) => {
+        didIOffer.current = true;
+        console.log(offer);
+        socket.current.emit('offer', offer);
+        connection.current.setLocalDescription(offer);
+      }).then(() => console.log("offerer set local description")).catch((reason) => console.log(reason));
+    }
+    else{
+      connection.current.setRemoteDescription(offer.current).then(() => console.log("answerer set remote description"));
+      connection.current.createAnswer().then((answer) => {
+        didIOffer.current = false;
+        console.log(answer);
+        socket.current.emit('answer', answer);
+        connection.current.setLocalDescription(answer);
+      }).then(() => console.log("answerer set local description")).catch((reason) => console.log(reason));
+    }
+
+    connection.current.addEventListener('track', (event) => {
+      // this fires 2 times: once for camera track, once for microphone track
+      console.log("event: "+event);
+      console.log("received track: "+event.track);  // mediastreamtrack object
+      console.log("content hint: "+event.track.contentHint);  // no output
+      console.log("streams: "+event.streams.length);  // length is 0
+      remoteTrack.current.addTrack(event.track);
+      remoteStream.current.srcObject = remoteTrack.current;
+    });
+  }
+  
+  return <>
+  <div id = "top-button-row">
+  <button onClick = {onOpenConnection}>{canOffer ? 'Open Connection' : 'Answer Connection'}</button>
+  <button>Close Connection</button>
+  </div>
+  <div>
+    <video ref = {localStream} controls autoPlay width = "300" height = "200"></video>
+    <video ref = {remoteStream} controls autoPlay width = "300" height = "200"></video>
+  </div>
+  </>;
 }
 
 export default App;
